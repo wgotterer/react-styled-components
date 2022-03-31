8c5e2927ad2512f23030f8fb98aae3d003c57059
William Gotterer <william.gotterer@gmail.com>
2022-03-31 16:55:15 -0500

    used tilde styling

--- src/components/common/PasswordInput.js
+++ src/components/common/PasswordInput.js
@@ -2,21 +2,52 @@ import { Input } from "components/common";
 import React, { useState } from "react";
 import styled from "styled-components";
 
+const PasswordInputWrapper = styled.div`
+  display: flex;
+/* the tilde grabs ahold of the next child element. in this case it is the div with the shown password */
+  ~div{
+      margin-bottom: 6px;
+  }
+`;
 
 // with the attrs we add anythign we want to add to this component
 // or in this case html element
 const PasswordInputStyle = styled(Input).attrs(() => ({
-    type: "password",
-    placeholder: "Password"
+  type: "password",
+  placeholder: "Password",
 }))`
-    border-top-right-radius: 0;
-    border-bottom-right-radius: 0;
-    `
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+`;
 
-export function PasswordInput(props){
+const ToggleButton = styled.div`
+  height: 40px;
+  border: 1px solid #ccc;
+  box-sizing: border-box;
+  font-size: 0.9em;
+  display: flex;
+  padding: 8px;
+  border-left: 0;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
+  background: white;
+  font-weight: bold;
+  cursor: pointer;
+  /* userselect unable to highlight text */
+  user-select: none;
+  color: black;
+`;
 
-    const [showPassword, setShowPassword] = useState(false)
-    return (
-       <PasswordInputStyle {...props} />
-    )
+export function PasswordInput(props) {
+  const [showPassword, setShowPassword] = useState(false);
+  return (
+    <>
+      <PasswordInputWrapper>
+        <PasswordInputStyle {...props} />
+        <ToggleButton onClick={() => setShowPassword(!showPassword)}>{showPassword ? "Hide" : "Show"}</ToggleButton>
+      </PasswordInputWrapper>
+
+      <div>{showPassword ? props.value : ""}</div>
+    </>
+  );
 }
